f03f6dbe86caa118ad759d1190f6ec58cfa80b5b
furkantok <furkant.ftk@gmail.com>
2022-04-10 20:28:33 +0300

    work 3 is done and about to be pushed to GitHub after commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,10 @@ public class HelloWorld {
 
     //doing awesome work 2!
 
+    //doing awesome work 3!
+    //awesome work 3 is done and ready to be committed
+    //and ready to to be pushed to remote repository
+
 
 }
 
